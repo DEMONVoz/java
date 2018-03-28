7240b5a1d978835d66f98b7ec54c9425626fcc58
DEMONVoz <35841180+DEMONVoz@users.noreply.github.com>
2018-03-28 13:53:01 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,22 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lab2;
-
 /**
- *
- * @author DEMON
- */
-public class Lab2 {
-
-    /**
-     * @param args the command line arguments
-     */
+ * Created by 1610504 on 3/28/2018.
+ */import java.util.Scanner;
+public class laba2 {
     public static void main(String[] args) {
-        double a = 0.01, x = 7.1, b = 5, c = 4.1;
-    double F = (Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
-            System.out.println("F = " + F);
+        Scanner in = new Scanner(System.in);
+        System.out.print("0 для заданих чисел або 1 для ввода з клавиатури : ");
+        int k = in.nextInt();
+        /**Введение чисел*/
+        if ( k == 0) {
+            int x = 2;
+            double a = 0.01;
+            double b = 5;
+            double c = 4.1;
+            System.out.println(Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
+        }
+        else if( k == 1)
+        {
+            System.out.print("Введите знаение х: ");
+            int x = in.nextInt();
+            System.out.print("Введите значения a: ");
+            double a = in.nextDouble();
+            System.out.print("Введите значения b: ");
+            double b = in.nextDouble();
+            System.out.print("Введите значения c: ");
+            double c = in.nextDouble();
+            System.out.println(Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
+        }
+        else{
+            System.out.println("error");
         }
     }
+}
