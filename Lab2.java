b1bed314fa9a902df69855244a71ac788b7d8a5b
DEMONVoz <35841180+DEMONVoz@users.noreply.github.com>
2018-03-29 15:43:13 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,30 +1,29 @@
-/**
- * Created by 1610504 on 3/28/2018.
- */import java.util.Scanner;
-public class laba2 {
+import java.util.Scanner;
+public class lab2_new {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("0 для заданих чисел або 1 для ввода з клавиатури : ");
         int k = in.nextInt();
+        double x = 2;
+        double a = 0.01;
+        double b = 5;
+        double c = 4.1;
+        double F = Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1);
         /**Введение чисел*/
-        if ( k == 0) {
-            int x = 2;
-            double a = 0.01;
-            double b = 5;
-            double c = 4.1;
-            System.out.println(Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
+        if (k == 0) {           
+            System.out.println(F + "F ");
         }
         else if( k == 1)
         {
             System.out.print("Введите знаение х: ");
-            int x = in.nextInt();
+            x = in.nextInt();
             System.out.print("Введите значения a: ");
-            double a = in.nextDouble();
+            a = in.nextDouble();
             System.out.print("Введите значения b: ");
-            double b = in.nextDouble();
+            b = in.nextDouble();
             System.out.print("Введите значения c: ");
-            double c = in.nextDouble();
-            System.out.println(Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
+            c = in.nextDouble();
+            System.out.println(F + "F ");
         }
         else{
             System.out.println("error");
