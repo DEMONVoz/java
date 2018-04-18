65e79d36e738a8c88c2613138e547538b2bd9cc9
DEMONVoz <35841180+DEMONVoz@users.noreply.github.com>
2018-04-18 12:39:55 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class laba2 {
+public class lab2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double x = 2;
