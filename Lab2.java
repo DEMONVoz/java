a02ad47432462895b5d1889e1ac7cc636ddd4cc4
DEMONVoz <35841180+DEMONVoz@users.noreply.github.com>
2018-04-18 12:37:09 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
-public class lab2_new {
+public class laba2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("0 для заданих чисел або 1 для ввода з клавиатури : ");
+        int x = 2;
+        double a = 0.75;
+        double b = 1.1;
+        double c = 0.4;
+        System.out.print("Введіть 0 для обчислення заданих чисел або 1 для вводу з клавіатури : ");
         int k = in.nextInt();
-        double x = 2;
-        double a = 0.01;
-        double b = 5;
-        double c = 4.1;
-        double F = Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1);
-        /**Введение чисел*/
-        if (k == 0) {           
-            System.out.println(F + "F ");
-        }
-        else if( k == 1)
+        if( k == 1)
         {
             System.out.print("Введите знаение х: ");
             x = in.nextInt();
@@ -23,10 +18,7 @@ public class lab2_new {
             b = in.nextDouble();
             System.out.print("Введите значения c: ");
             c = in.nextDouble();
-            System.out.println(F + "F ");
         }
-        else{
-            System.out.println("error");
-        }
+        System.out.println(Math.exp(-a*x) * Math.cos(b*x+c) + Math.exp(a*x) + Math.sin(c*x-1));
     }
 }
